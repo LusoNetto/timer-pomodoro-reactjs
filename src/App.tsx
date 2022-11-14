87a85380f7d1a5158d4a974615a5387a757aee64
LusoNetto <luciano.silva137@hotmail.com>
2022-11-14 13:37:25 -0300

    configure eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import {ThemeProvider} from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 
 import { Button } from './components/Button'
 import { defaultTheme } from './themes/default'
 import { GlobalStyle } from './themes/global'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant='primary'/>
-      <Button variant='secundary'/>
-      <Button variant='danger'/>
-      <Button variant='sucess'/>
-      <Button/>
-      <GlobalStyle/>
+      <Button variant="primary" />
+      <Button variant="secundary" />
+      <Button variant="danger" />
+      <Button variant="sucess" />
+      <Button />
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,32 +1,32 @@
-import styled, {css} from "styled-components";
+import styled, { css } from 'styled-components'
 
-export type ButtonVariant = 'primary'|'secundary'|'danger'|'sucess';
+export type ButtonVariant = 'primary' | 'secundary' | 'danger' | 'sucess'
 
-interface ButtonContainer{
-    variant: ButtonVariant;
+interface ButtonContainer {
+  variant: ButtonVariant
 }
 
 const buttonVariants = {
-    primary: 'white',
-    secundary: 'gray',
-    danger: 'red',
-    sucess: 'green'
-};
+  primary: 'white',
+  secundary: 'gray',
+  danger: 'red',
+  sucess: 'green',
+}
 
 export const ButtonContainer = styled.button<ButtonContainer>`
-    width: 100px;
-    height: 40px;
-    border-radius: 4px;
-    border: 0;
-    margin: 8px;
+  width: 100px;
+  height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-    background-color: ${props => props.theme["green-500"]};
-    color: ${props => props.theme.white};
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
 
-    /*${props => {
-        return css`
-            background-color: ${buttonVariants[props.variant]}
-        `
-    }}
+  /*${(props) => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `
+  }}
     */
 `
