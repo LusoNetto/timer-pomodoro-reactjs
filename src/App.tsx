52fee50609c2f6d2601d4e1d76c3d65412a5d834
LusoNetto <luciano.silva137@hotmail.com>
2022-11-15 10:41:20 -0300

    add route layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-
-import { Button } from './components/Button'
+import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './themes/default'
 import { GlobalStyle } from './themes/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secundary" />
-      <Button variant="danger" />
-      <Button variant="sucess" />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
